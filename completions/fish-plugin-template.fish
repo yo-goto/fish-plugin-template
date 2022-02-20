efa520fbd0b995450694c73049c2cc6e088517f9
yo-goto <50942816+yo-goto@users.noreply.github.com>
2022-02-21 02:10:13 +0900

    added new completions for directories & files

--- completions/fish-plugin-template.fish
+++ completions/fish-plugin-template.fish
@@ -1,6 +1,6 @@
-# generated completion template from fish-plugin
 complete -c fish-plugin-template -s v -l version -f -d "Show version info"
 complete -c fish-plugin-template -s h -l help -f -d "Show help"
 complete -c fish-plugin-template -s d -l debug -f -d "Debug"
 complete -c fish-plugin-template -s p -l project -f -d "Make project files"
 complete -c fish-plugin-template -s a -l add_template -f -d "Add template to a file"
+complete -c fish-plugin-template -n __fish_use_subcommand -xa 'functions completions conf.d README LICENSE CHANGELOG'
