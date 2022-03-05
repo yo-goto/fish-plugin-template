d6063efdb17119253e25b292eb6c5f139d16ce0e
yo-goto <50942816+yo-goto@users.noreply.github.com>
2022-03-05 18:12:56 +0900

    updated completions: added minimal option & erased add_template option

--- completions/fish-plugin-template.fish
+++ completions/fish-plugin-template.fish
@@ -1,6 +1,7 @@
 complete -c fish-plugin-template -s v -l version -f -d "Show version info"
 complete -c fish-plugin-template -s h -l help -f -d "Show help"
 complete -c fish-plugin-template -s d -l debug -f -d "Debug"
+complete -c fish-plugin-template -s n -l no_template -f -d "Disable adding templates"
+complete -c fish-plugin-template -s m -l minimal -f -d "Make a minimal template set (function completion CHANGELOG)"
 complete -c fish-plugin-template -s p -l project -f -d "Make project files"
-complete -c fish-plugin-template -s a -l add_template -f -d "Add template to a file"
 complete -c fish-plugin-template -n __fish_use_subcommand -xa 'functions completions conf.d README LICENSE CHANGELOG'
